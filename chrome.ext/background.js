e9bbe251dd0ad562a235f56b574a0d78d86c1311
popcat <moviemaker7@gmail.com>
2016-02-20 14:38:28 -0500

    added native messaging functionality

--- chrome.ext/background.js
+++ chrome.ext/background.js
@@ -1,12 +1,35 @@
-//listens for when an existing tab becomes active or new tab opened
-chrome.tabs.onActivated.addListener(function(activeInfo){
-	getTabObjectForTabId(activeInfo.tabId);
-});
+var application = "com.kelhop.HackIllinois.HackIllinoisChromeExtension";
+var port = null;
 
-//listens for when an existing tab's url changes
-chrome.tabs.onUpdated.addListener(function(tabId){
-	getTabObjectForTabId(tabId);
-});
+(function() {
+	initNativeMessaging();
+	createPageOpenOrChangeListeners();
+})();
+
+function initNativeMessaging(){
+	port = chrome.runtime.connectNative(application);
+
+	port.onMessage.addListener(function(msg){
+		console.log(msg);
+	});
+
+	port.onDisconnect.addListener(function(e) {
+	    console.log('unexpected disconnect');
+	    port = null;
+	});
+}
+
+function createPageOpenOrChangeListeners(){
+	//listens for when an existing tab becomes active or new tab opened
+	chrome.tabs.onActivated.addListener(function(activeInfo){
+		getTabObjectForTabId(activeInfo.tabId);
+	});
+
+	//listens for when an existing tab's url changes
+	chrome.tabs.onUpdated.addListener(function(tabId){
+		getTabObjectForTabId(tabId);
+	});
+}
 
 //Matches the tab id to the tab object (which contains the url)
 function getTabObjectForTabId(tabId){
@@ -14,8 +37,17 @@ function getTabObjectForTabId(tabId){
 	    for (var i = 0; i < tabs.length; i++) {
 	    	if(tabs[i].id == tabId){
 	    		console.log(tabs[i].url);
+	    		sendUrlToApp();
 	    	}                    
 	    }
 	});
 }
 
+function sendUrlToApp(){
+	if(!port){
+		console.log("No connection to app: message not sent");
+		return;
+	}
+	chrome.runtime.sendNativeMessage(application, "test", null );
+
+}
