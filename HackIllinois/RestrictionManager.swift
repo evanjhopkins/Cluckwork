39c9d38bf9dd621e564651b264b51bb76763dd8c
popcat <moviemaker7@gmail.com>
2016-02-20 21:35:47 -0500

    added checking on app restrictions

--- HackIllinois/RestrictionManager.swift
+++ HackIllinois/RestrictionManager.swift
@@ -10,16 +10,22 @@ import Cocoa
 
 class RestrictionManager: NSObject, FocusManagerDelegate {
     var restrictionProfile: RestrictionProfile
-    //var restrictedDomains:[String:Bool] = ["www.facebook.com":false, "reddit.com":false]
-    //var restrictedApps:[String:Bool] = ["Spotify":false]
-    
+
     init(restrictionProfile: RestrictionProfile) {
         self.restrictionProfile = restrictionProfile
     }
     
-    func isUrlAllowed(raw_url:String) -> Bool {
+    func isWebsiteAllowed(raw_url:String) -> Bool {
         let domain = domainFromUrl(raw_url)
         if(restrictionProfile.restrictedWebsites[domain] == false){
+            return false
+        }else{
+            return true
+        }
+    }
+    
+    func isAppAllowed(app_name:String) -> Bool {
+        if(restrictionProfile.restrictedApps[app_name] == false){
             return false
         }else{
             return true
@@ -34,10 +40,12 @@ class RestrictionManager: NSObject, FocusManagerDelegate {
     
     func focusManager(focusManager: FocusManager, didChangeToApplication runningApplication: NSRunningApplication) {
         //        self.focusedAppNameLabel.stringValue = runningApplication.bundleIdentifier ?? "<<<error>>>"
+        print(runningApplication.localizedName! + " -> " + isAppAllowed(runningApplication.localizedName!).description)
+
     }
     
     func focusManager(focusManager: FocusManager, didChangeToWebsiteURL websiteURL: NSURL) {
-        print(websiteURL.description + " -> " + isUrlAllowed(websiteURL.description).description)
+        print(domainFromUrl(websiteURL.description) + " -> " + isWebsiteAllowed(websiteURL.description).description)
         //        self.focusedAppNameLabel.stringValue = websiteURL.path ?? "<<<error>>>"
     }
 }
