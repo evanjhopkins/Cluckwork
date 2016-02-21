854712389d66ee6c678737255c2733a22e08f700
popcat <moviemaker7@gmail.com>
2016-02-20 21:34:10 -0500

    create restrictionProfile from json file

--- HackIllinois/RestrictionProfile.swift
+++ HackIllinois/RestrictionProfile.swift
@@ -15,35 +15,35 @@ class RestrictionProfile: NSObject{
     var isWhiteListApps: Bool
     var isWhiteListWebsites: Bool
     
-    //new
     init(profileName: String, restrictedWebsites: [String:Bool], restrictedApps: [String:Bool], isWhiteListApps: Bool, isWhiteListWebsites: Bool) {
         self.profileName = profileName
         self.restrictedWebsites = restrictedWebsites
         self.restrictedApps = restrictedApps
         self.isWhiteListApps = isWhiteListApps
         self.isWhiteListWebsites = isWhiteListWebsites
-        RestrictionProfile.loadRestrictionProfileFromFile("test_profile")
     }
     
-    static func loadRestrictionProfileFromFile(profileName: String) {
-        if let path = NSBundle.mainBundle().pathForResource("test_profile", ofType: "json") {
+    class func RestrictionProfileFromFile(profileName: String) -> RestrictionProfile? {
+        if let path = NSBundle.mainBundle().pathForResource(profileName, ofType: "json") {
             do {
                 let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
                 let jsonObj = JSON(data: data)
                 if jsonObj != JSON.null {
-                    print(jsonObj["restrictedApps"])
-                    let profileName = jsonObj["profileName"]
-                    let restrictedApps = jsonObj["restrictedApps"]
-                    let restrictedWebsites = jsonObj["restrictedWebsites"]
-                    let isWhiteListApps = jsonObj["isWhiteListApps"]
-                    let isWhiteListWebsites = jsonObj["isWhiteListWebsites"]
-//                    let loadedRestrictionProfile = RestrictionProfile(
-//                        profileName: profileName.stringValue,
-//                        restrictedWebsites: restrictedWebsites.dictionaryValue,
-//                        restrictedApps: restrictedApps.dictionaryValue,
-//                        isWhiteListApps: isWhiteListApps.boolValue,
-//                        isWhiteListWebsites: isWhiteListWebsites.boolValue
-//                    )
+                    //coverting json data to native objects
+                    let profileName = jsonObj["profileName"].stringValue
+                    let restrictedApps = jsonObj["restrictedApps"].dictionaryObject as! [String:Bool]
+                    let restrictedWebsites = jsonObj["restrictedWebsites"].dictionaryObject as! [String:Bool]
+                    let isWhiteListApps = jsonObj["isWhiteListApps"].boolValue
+                    let isWhiteListWebsites = jsonObj["isWhiteListWebsites"].boolValue
+                    //create a restriction profile from those objects
+                    let loadedRestrictionProfile = RestrictionProfile(
+                        profileName: profileName,
+                        restrictedWebsites: restrictedWebsites,
+                        restrictedApps: restrictedApps,
+                        isWhiteListApps: isWhiteListApps,
+                        isWhiteListWebsites: isWhiteListWebsites
+                    )
+                    return loadedRestrictionProfile
                 } else {
                     print("File was found, but appears to be invalid")
                 }
@@ -53,9 +53,15 @@ class RestrictionProfile: NSObject{
         } else {
             print("File not found, invalid filename/path.")
         }
+        return nil
+    }
+    
+    class func getProfiles() -> [String] {
+        let profiles: [String] = ["test_profile", "work_profile", "study_profile"]
+        return profiles
     }
     
     func saveRestrictionProfileToFile() {
-    
+        //not implementing for demo
     }
 }
